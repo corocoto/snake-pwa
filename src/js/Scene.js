e808794c6080d7744109e91e4c229b93930c2a47
Artem Gusev <gusev2014russia@mail.ru>
2021-01-02 16:02:57 +0300

    adds basic functionality for Snake class

--- src/js/Scene.js
+++ src/js/Scene.js
@@ -2,6 +2,7 @@ const BG_COLOR = '#CE9752';
 const SCALE = 20;
 
 import Apple from './Apple.js';
+import Snake from './Snake.js';
 
 export default class Scene{
     constructor(canvasRef) {
@@ -11,7 +12,13 @@ export default class Scene{
 
         this.setSceneSize();
         this.AppleInst = new Apple(this.ctx, SCALE);
-        this.AppleInst.generatePosition();
+        this.SnakeInst = new Snake(this.ctx, SCALE, SCALE);
+        
+        while(this.AppleInst.xPos === this.SnakeInst.xPos && this.AppleInst.yPos === this.SnakeInst.yPos){
+            this.AppleInst.generatePosition();
+            this.SnakeInst.generatePosition();
+        } 
+
         this.render();
     }
 
@@ -39,6 +46,7 @@ export default class Scene{
         this.ctx.fillStyle = BG_COLOR;
         this.ctx.fillRect(0, 0, this.width, this.height);
         this.AppleInst.render();
+        this.SnakeInst.render();
         window.requestAnimationFrame(this.render);
     }
 
--- src/js/Snake.js
+++ src/js/Snake.js
@@ -1,10 +1,64 @@
 const SNAKE_COLOR = '#017D89';
 
+
 export default class Snake {
-    constructor(width = 20, height = 20) {
+    constructor(ctx, width, height) {
+        this.ctx = ctx;
         this.width = width;
         this.height = height;
+
+        this.xPos = 0;
+        this.yPos = 0;
+        this.keyDownEventHandler = this.keyDownEventHandler.bind(this);
+        window.addEventListener('keydown', this.keyDownEventHandler);
+        this.turnOn = 'RIGHT';
     }
 
-    // change
+    generatePosition() {
+        const maxW = window.innerWidth - this.width;
+        const maxH = window.innerHeight - this.height;
+
+        this.xPos = Math.floor(Math.random() * (maxW + 1));
+        this.yPos = Math.floor(Math.random() * (maxH + 1));
+    }
+
+    render(){
+        this.ctx.fillStyle = SNAKE_COLOR;
+        this.ctx.fillRect(this.xPos, this.yPos, this.width, this.height);
+        switch(this.turnOn) {
+            case 'RIGHT':
+                this.xPos += this.width;
+                break;
+            case 'LEFT':
+                this.xPos -= this.width;
+                break;
+            case 'TOP':
+                this.yPos -= this.width;
+                break;
+            case 'BOTTOM':
+                this.yPos += this.width;  
+                break;          
+        }
+    }
+
+    keyDownEventHandler({keyCode}) {
+        switch (keyCode){
+            case 38:
+            case 87:    
+                this.turnOn = 'TOP';
+                break;
+            case 39:
+            case 68:
+                this.turnOn = 'RIGHT';
+                break;
+            case 40:
+            case 83:
+                this.turnOn = 'BOTTOM';
+                break;
+            case 37:
+            case 65:
+                this.turnOn = 'LEFT';
+                break;                        
+        }
+    }
 }
